af553161c93eba9ab1944c4fdb84eaaa4b544443
hariharanragothaman <hariharanragothaman@gmail.com>
2020-02-04 21:33:00 -0500

    Polishing

--- iss_tracking.py
+++ iss_tracking.py
@@ -1,3 +1,4 @@
+import time
 import json
 import argparse
 import requests
@@ -12,7 +13,14 @@ class ISSTracking(object):
         :brief: Constructor of Class
         :param url: URL of the OpenNotify API
         """
-        print("Entering the Constructor")
+
+    def epoch_time_converter(self, epochtime):
+        """
+        :brief: Function to convert epoch time
+        :return (str): time in string format
+        """
+        time_human_format = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(epochtime))
+        return time_human_format
 
     def get_iss_location(self):
         """
@@ -22,7 +30,8 @@ class ISSTracking(object):
         iss_object = requests.get('http://api.open-notify.org/iss-now.json', auth=('user', 'pass'))
         response = iss_object.text
         response_dict = json.loads(response)
-        print('The ISS current location at {} is {} {}'.format(response_dict["timestamp"],
+        time_stamp = self.epoch_time_converter(response_dict["timestamp"])
+        print('The ISS current location at {} is {} {}'.format(time_stamp,
                                                                response_dict["iss_position"]["latitude"],
                                                                response_dict["iss_position"]["longitude"]))
         return response
@@ -30,21 +39,33 @@ class ISSTracking(object):
     def get_pass_times(self, latitude, longitude, altitude=None, number=None):
         """
         :brief: Function get overhead passing time of ISS
-        :param latitude:   Latitude of the place to predict passes
-        :param longitude:  Longitude of the place to predict passes
-        :param altitude:   Altitude of the place to predict passes
+        :param latitude:   Latitude of the place to predict passes  (degress)
+        :param longitude:  Longitude of the place to predict passes (degrees)
+        :param altitude:   Altitude of the place to predict passes  (metres)
         :param number:     Number of passes to return
         """
         iss_url = "http://api.open-notify.org/iss-pass.json?lat=" + latitude + "&lon=" + longitude
         iss_object = requests.get(iss_url)
         response = iss_object.text
+        pass_times = json.loads(response)
+        for res in pass_times["response"]:
+            print('The ISS will be overhead {} {} at {} for {} seconds'.format(latitude,
+                                                                       longitude,
+                                                                       self.epoch_time_converter(res["risetime"]),
+                                                                       res["duration"]))
+        #"The ISS will be overhead {LAT, LONG} at {time} for {duration}”"
         return response
 
 if __name__ == "__main__":
     iss = ISSTracking()
+    """
+    Function call to current ISS location
+    """
     response = iss.get_iss_location()
+    """
+    Function call to get ISS Pass times
+    """
     response_dict = json.loads(response)
     latitude = response_dict["iss_position"]["latitude"]
     longitude = response_dict["iss_position"]["longitude"]
-    pass_times = iss.get_pass_times(latitude, longitude)
-    print(pass_times)
+    iss.get_pass_times(latitude, longitude)

8e67ef88d29c894c2b9d3bfa41bfbc42307ab93b
hariharanragothaman <hariharanragothaman@gmail.com>
2020-02-04 21:58:05 -0500

    Adding sanity checks and useful comments

--- iss_tracking.py
+++ iss_tracking.py
@@ -43,6 +43,16 @@ class ISSTracking(object):
         :param altitude:   Altitude of the place to predict passes  (metres)
         :param number:     Number of passes to return
         """
+        # SANITY CHECKS
+        if abs(float(latitude)) > 80 or abs(float(longitude)) > 180:
+               raise ValueError("Invalid Input: Please enter input of a valid range")
+        if altitude is not None:
+            if (int(altitude) < 0 or int(altitude) > 10000):
+               raise ValueError("Invalid Input: Please enter input of a valid range")
+        if number is not None:
+            if int(number) < 0 or int(number) > 100:
+               raise ValueError("Invalid Input: Please enter input of a valid range")
+
         iss_url = "http://api.open-notify.org/iss-pass.json?lat=" + latitude + "&lon=" + longitude
         iss_object = requests.get(iss_url)
         response = iss_object.text
@@ -52,19 +62,18 @@ class ISSTracking(object):
                                                                        longitude,
                                                                        self.epoch_time_converter(res["risetime"]),
                                                                        res["duration"]))
-        #"The ISS will be overhead {LAT, LONG} at {time} for {duration}”"
         return response
 
 if __name__ == "__main__":
+    """
+    TODO: Run pylint
+          Write some unit-tests
+    """
     iss = ISSTracking()
-    """
-    Function call to current ISS location
-    """
+    # Driver code for getting ISS location
     response = iss.get_iss_location()
-    """
-    Function call to get ISS Pass times
-    """
     response_dict = json.loads(response)
     latitude = response_dict["iss_position"]["latitude"]
     longitude = response_dict["iss_position"]["longitude"]
+    # Driver code for getting ISS passing overhead time
     iss.get_pass_times(latitude, longitude)

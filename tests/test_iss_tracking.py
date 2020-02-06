882f9bd19a6e04d176c1c93bdcda36663b4c5b7a
hariharanragothaman <hariharanragothaman@gmail.com>
2020-02-06 17:00:43 -0500

    Adding basic unit-tests

--- tests/test_iss_tracking.py
+++ tests/test_iss_tracking.py
@@ -6,10 +6,15 @@ from ..src.iss_tracking import ISSTracking
 iss_object = ISSTracking()
 
 def test_iss_location():
-
+    location = iss_object.get_iss_location()
+    assert location
 
 def test_pass_time():
-
+    latitiude = 20
+    longitud e= 30
+    pass_time = iss_object.get_pass_times()
+    assert pass_time
 
 def test_ppl_in_space():
-
+    num_ppl_in_space = iss_object.get_ppl_in_space()
+    assert num_ppl_in_space
